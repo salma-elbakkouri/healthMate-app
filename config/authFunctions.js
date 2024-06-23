5b945eb4233d9de44ea0a6a36a6342c638fbd7ce
salma el bakkouri <86151871+salma-elbakkouri@users.noreply.github.com>
2024-06-23 23:38:39 +0100

    Update authentication functions and added notif scheduling and fetching

--- config/authFunctions.js
+++ config/authFunctions.js
@@ -3,7 +3,9 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, se
 import { Alert } from 'react-native';
 import { doc, setDoc, getDoc, getDocs, collection, query, where, deleteDoc, updateDoc } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { getDatabase, ref, push, set } from 'firebase/database';
+import { scheduleNotification } from '../components/notifsetup';
+import moment from 'moment-timezone';
 // Login user
 export const loginUser = async (email, password) => {
   try {
@@ -34,7 +36,6 @@ export const forgotPassword = async (email) => {
   }
 };
 
-
 export const registerUser = async (fullName, email, password, imageIndex = 0) => {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
@@ -61,7 +62,65 @@ export const registerUser = async (fullName, email, password, imageIndex = 0) =>
   }
 };
 
+const calculateTimeUntil = (timeString) => {
+  const [time, modifier] = timeString.split(' ');
+  let [hours, minutes] = time.split(':');
+  if (modifier === 'PM' && hours !== '12') {
+    hours = parseInt(hours, 10) + 12;
+  }
+  if (modifier === 'AM' && hours === '12') {
+    hours = 0;
+  }
+  const now = new Date();
+  const targetTime = new Date();
+  targetTime.setHours(hours, minutes, 0);
 
+  if (targetTime < now) {
+    targetTime.setDate(now.getDate() + 1);
+  }
+
+  return (targetTime - now) / 1000; // seconds until the target time
+};
+
+export const fetchUserNotifications = async (userId) => {
+  try {
+    const notificationsQuery = query(
+      collection(firestore, 'notifications'),
+      where('userId', '==', userId)
+    );
+    const querySnapshot = await getDocs(notificationsQuery);
+    const notifications = [];
+    querySnapshot.forEach((doc) => {
+      notifications.push({ id: doc.id, ...doc.data() });
+    });
+    return notifications;
+  } catch (error) {
+    console.error('Error fetching notifications:', error);
+    throw new Error('Failed to fetch notifications');
+  }
+};
+
+export const saveNotification = async (userId, pillName, dose, time) => {
+  try {
+    const notificationTime = moment(time, 'h:mm A').tz('Africa/Casablanca').toISOString();
+    const notification = {
+      userId,
+      title: `${pillName}`,
+      body: `${dose} at ${time}.`,
+      pillName,
+      dose,
+      time,
+      timestamp: notificationTime, // Use the notification time for the timestamp
+    };
+
+    await setDoc(doc(collection(firestore, 'notifications')), notification);
+
+    console.log('Notification saved successfully');
+  } catch (error) {
+    console.error('Error saving notification:', error);
+    throw new Error('Failed to save notification');
+  }
+};
 
 export const saveMedicineReminder = async (userId, pillName, amount, frequency, beginDate, endDate, doses, notificationEnabled) => {
   try {
@@ -70,9 +129,9 @@ export const saveMedicineReminder = async (userId, pillName, amount, frequency, 
       pillName: pillName,
       amount: amount,
       frequency: frequency,
-      beginDate: beginDate.toISOString(), // Convert to ISO string
-      endDate: endDate.toISOString(), // Convert to ISO string
-      notificationEnabled: notificationEnabled
+      beginDate: beginDate.toISOString(),
+      endDate: endDate.toISOString(),
+      notificationEnabled: notificationEnabled,
     };
 
     // Add doses based on the frequency
@@ -81,21 +140,31 @@ export const saveMedicineReminder = async (userId, pillName, amount, frequency, 
     if (times >= 2) data.secondDose = doses.secondDose;
     if (times >= 3) data.thirdDose = doses.thirdDose;
 
-    await setDoc(doc(firestore, 'medicines', `${userId}_${pillName}`), data);
+    await setDoc(doc(collection(firestore, 'medicineReminders')), data);
+
+    // Save notifications if enabled
+    if (notificationEnabled) {
+      const dosesArray = Object.keys(doses).map(key => doses[key]);
+      for (let i = 0; i < dosesArray.length; i++) {
+        const doseTime = dosesArray[i];
+        if (doseTime) {
+          const secondsUntilDose = calculateTimeUntil(doseTime);
+          await saveNotification(userId, pillName, `dose ${i + 1}`, doseTime);
+          await scheduleNotification(`Time to take your medicine: ${pillName}`, `It's time to take your dose ${i + 1} of ${pillName} at ${doseTime}.`, secondsUntilDose, userId, pillName);
+        }
+      }
+    }
 
     console.log('Medicine reminder saved successfully');
-    Alert.alert('Success', 'Medicine reminder saved successfully.');
   } catch (error) {
     console.error('Error saving medicine reminder:', error);
-    Alert.alert('Error', 'Failed to save medicine reminder. Please try again.');
+    throw new Error('Failed to save medicine reminder');
   }
 };
 
-
-
 export const fetchMedicineReminders = async (userId) => {
   try {
-    const q = query(collection(firestore, 'medicines'), where('userId', '==', userId));
+    const q = query(collection(firestore, 'medicineReminders'), where('userId', '==', userId));
     const querySnapshot = await getDocs(q);
     const medicines = [];
     querySnapshot.forEach((doc) => {
@@ -115,7 +184,7 @@ export const fetchMedicineReminders = async (userId) => {
 
 export const deleteMedicineReminder = async (docId) => {
   try {
-    await deleteDoc(doc(firestore, 'medicines', docId));
+    await deleteDoc(doc(firestore, 'medicineReminders', docId));
     Alert.alert('Success', 'Medicine reminder deleted successfully.');
   } catch (error) {
     console.error('Error deleting medicine reminder:', error);
@@ -152,13 +221,26 @@ export const fetchMedicineReminderById = async (reminderId) => {
 // Update a specific medicine reminder
 export const updateMedicineReminder = async (reminderId, userId, pillName, amount, frequency, beginDate, endDate, doses, notificationEnabled) => {
   try {
+    // Fetch existing notifications related to this reminder
+    const notificationsQuery = query(
+      collection(firestore, 'notifications'),
+      where('userId', '==', userId),
+      where('pillName', '==', pillName)
+    );
+    const notificationsSnapshot = await getDocs(notificationsQuery);
+
+    // Delete existing notifications
+    const deletePromises = notificationsSnapshot.docs.map(doc => deleteDoc(doc.ref));
+    await Promise.all(deletePromises);
+
+    // Prepare updated reminder data
     const data = {
       userId: userId,
       pillName: pillName,
       amount: amount,
       frequency: frequency,
-      beginDate: beginDate.toISOString(), // Convert to ISO string
-      endDate: endDate.toISOString(), // Convert to ISO string
+      beginDate: beginDate.toISOString(),
+      endDate: endDate.toISOString(),
       notificationEnabled: notificationEnabled
     };
 
@@ -168,7 +250,21 @@ export const updateMedicineReminder = async (reminderId, userId, pillName, amoun
     if (times >= 2) data.secondDose = doses.secondDose;
     if (times >= 3) data.thirdDose = doses.thirdDose;
 
-    await updateDoc(doc(firestore, 'medicines', reminderId), data);
+    // Update the reminder in Firestore
+    await updateDoc(doc(firestore, 'medicineReminders', reminderId), data);
+
+    // Save notifications if enabled
+    if (notificationEnabled) {
+      const dosesArray = Object.keys(doses).map(key => doses[key]);
+      for (let i = 0; i < dosesArray.length; i++) {
+        const doseTime = dosesArray[i];
+        if (doseTime) {
+          const secondsUntilDose = calculateTimeUntil(doseTime);
+          await saveNotification(userId, pillName, `dose ${i + 1}`, doseTime);
+          await scheduleNotification(`Time to take your medicine: ${pillName}`, `It's time to take your dose ${i + 1} of ${pillName} at ${doseTime}.`, secondsUntilDose, userId, pillName);
+        }
+      }
+    }
 
     console.log('Medicine reminder updated successfully');
     Alert.alert('Success', 'Medicine reminder updated successfully.');
